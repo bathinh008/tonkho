2b3bfeaf043c7b94b36ff4586dd2ca7118e7cded
bathinh008 <thinhphuongba@gmail.com>
2025-11-26 09:18:58 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const sheetCSV = "https://docs.google.com/spreadsheets/d/e/2PACX-1vXXXXXXX/pub?output=csv";
+const sheetCSV = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQs5iOdYRcQ_ekDgPPzIw7FRwN1tiF7hY3YWPAw3_6ga6xUMt-SgeiNzSMpVotjUypdAAZUAvRfReAu/pub?gid=0&single=true&output=csv";
 
 async function loadInventory() {
     const tableDiv = document.getElementById("table");
@@ -74,3 +74,4 @@ function search(keyword) {
 }
 
 loadInventory();
+
